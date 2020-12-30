d1546c45a5309149dd2b9fb371236150c0062319
Janmejay Buranpuri <jburanpu@uwo.ca>
2020-12-30 14:45:30 -0500

    Created routes for courses, schedule and users api

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const connectToDb = require('./config/db');
+
+const app = express();
+
+//for aws or local host
+const PORT = process.env.PORT || 5000;
+
+connectToDb(); //to connect to database
+
+app.use(express.json({ extended: false }));
+
+app.use(cors());
+//routes for app.use
+app.use('/api/users', require('./api/users'));
+app.use('/api/courses', require('./api/courses'));
+app.use('/api/schedule', require('./api/schedule'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => (`Server started on port ${PORT}`));
